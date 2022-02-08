e21036cc7b483d8b96b24ae859d27e9ac6383f61
natasha.lerzer <natasha.lerzer@brandad-systems.de>
2022-02-08 11:51:58 +0100

    changed the template form to reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import { HomeComponent } from './home/home.component';
+import {AutoFocus} from "./utils/auto-focus.directive";
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    AutoFocus
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {LoginModel} from "../../model/login.model";
 import {LoginService} from "../services/login.service";
-import {HomeComponent} from "../home/home.component";
 import {Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 
 @Component({
@@ -12,23 +12,30 @@ import {Router} from "@angular/router";
 })
 export class LoginComponent implements OnInit {
 
-  errorMessage :string = '';
+  errorMessage: string = '';
+  loginForm: FormGroup = this.formBuilder.group({
+    username: ['',Validators.required],
+    password:['', Validators.required]
+  });
 
+  loginModel: LoginModel = {username: '', password: ''};
 
-  loginModel:LoginModel = {username:'', password:''};
-
-  constructor(private readonly loginService: LoginService, private readonly router: Router) { }
-
-  ngOnInit(): void {
+  constructor(private readonly loginService: LoginService, private readonly router: Router, private formBuilder: FormBuilder) {
   }
 
-  onSubmit():void{
+  ngOnInit(): void {
+
+  }
+
+  onSubmit(): void {
+    this.loginModel.username = this.loginForm.get('username')?.value;
+    this.loginModel.password = this.loginForm.get('password')?.value;
     this.loginService.login(this.loginModel).subscribe(result => {
-      console.log(result);
-      this.router.navigate(['home']);
-      this.errorMessage = '';
-    },
-        error =>  this.errorMessage ='Leider konnten wir Sie nicht einloggen, ' +
-          'da Sie falsche Daten eingegeben haben. Bitte überprüfen Sie Ihre Angaben noch einmal.' );
+        console.log(result);
+        this.router.navigate(['home']);
+        this.errorMessage = '';
+      },
+      error => this.errorMessage = 'Leider konnten wir Sie nicht einloggen, ' +
+        'da Sie falsche Daten eingegeben haben. Bitte überprüfen Sie Ihre Angaben noch einmal.');
   }
 }
