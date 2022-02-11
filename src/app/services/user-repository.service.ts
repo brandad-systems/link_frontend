26814bf7fb1ed8a4338f12cd8a7f3a4a26adc980
Tanja Maier (DATEV) <tanja.maier@brandad-systems.de>
2022-02-11 11:48:20 +0100

    check auth token in backend in isAuthenticated WIP

--- src/app/services/user-repository.service.ts
+++ src/app/services/user-repository.service.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpStatusCode} from "@angular/common/http";
 import {LoginModel} from "../../model/login.model";
 import {LoginResponseModel} from "../../model/login-response.model";
 import {Observable, map} from "rxjs";
 import { JwtHelperService } from '@auth0/angular-jwt';
+import {UserModel} from "../../model/user.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserRepositoryService {
 
-  baseUrl: string = "http://localhost:8080/api/public/login";
+  userModel!: UserModel;
+  baseUrl: string = "http://localhost:8080/api";
   constructor(private readonly httpClient: HttpClient, private readonly jwtHelper: JwtHelperService) { }
 
   public login(loginModel: LoginModel): Observable<LoginResponseModel> {
-    return this.httpClient.post<LoginResponseModel>(this.baseUrl, loginModel, { observe: 'response'}).pipe(map(response => {
+    return this.httpClient.post<LoginResponseModel>(`${this.baseUrl}/public/login`, loginModel, { observe: 'response'}).pipe(map(response => {
       const authKey = response.headers.get("Authorization")!;
       let body = response.body!;
       localStorage.setItem('token', authKey);
@@ -27,13 +29,32 @@ export class UserRepositoryService {
   public async isAuthenticated(): Promise<boolean> {
     const token = localStorage.getItem('token');
     if(token) {
-      //Token valid
-      return !this.jwtHelper.isTokenExpired(token);
+
+      if(!this.jwtHelper.isTokenExpired(token)){
+
+        this.httpClient.get<UserModel>(`${this.baseUrl}/admin/user/details`,
+          { observe: 'response', headers: {'Authorization': "Bearer " + token }})
+          .subscribe(response => {
+          let body = response.body!;
+          this.userModel.id = body.id;
+          this.userModel.username = body.username;
+          this.userModel.fullName = body.fullName;
+          if(response.status == HttpStatusCode.Ok){
+            return true;
+          }
+          return false;
+        });
+      }
+      else {
+        return false;
+      }
       //TODO am Backend nach validität des Users Fragen
+
     } else {
       return false;
     }
   }
+
 
 
 }
