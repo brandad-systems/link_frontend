acbd9077ad603b867f8ed44edd3418511a9ca907
natasha.lerzer <natasha.lerzer@brandad-systems.de>
2022-02-11 13:33:55 +0100

    validation of user on existing token

--- src/app/services/user-repository.service.ts
+++ src/app/services/user-repository.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
-import {HttpClient, HttpStatusCode} from "@angular/common/http";
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 import {LoginModel} from "../../model/login.model";
 import {LoginResponseModel} from "../../model/login-response.model";
-import {Observable, map} from "rxjs";
-import { JwtHelperService } from '@auth0/angular-jwt';
+import {catchError, lastValueFrom, map, Observable, of} from "rxjs";
+import {JwtHelperService} from '@auth0/angular-jwt';
 import {UserModel} from "../../model/user.model";
 
 @Injectable({
@@ -11,12 +11,15 @@ import {UserModel} from "../../model/user.model";
 })
 export class UserRepositoryService {
 
-  userModel!: UserModel;
+  userModel: UserModel = {id: '', username: '', fullName: ''};
   baseUrl: string = "http://localhost:8080/api";
-  constructor(private readonly httpClient: HttpClient, private readonly jwtHelper: JwtHelperService) { }
+
+
+  constructor(private readonly httpClient: HttpClient, private readonly jwtHelper: JwtHelperService) {
+  }
 
   public login(loginModel: LoginModel): Observable<LoginResponseModel> {
-    return this.httpClient.post<LoginResponseModel>(`${this.baseUrl}/public/login`, loginModel, { observe: 'response'}).pipe(map(response => {
+    return this.httpClient.post<LoginResponseModel>(`${this.baseUrl}/public/login`, loginModel, {observe: 'response'}).pipe(map(response => {
       const authKey = response.headers.get("Authorization")!;
       let body = response.body!;
       localStorage.setItem('token', authKey);
@@ -28,33 +31,20 @@ export class UserRepositoryService {
 
   public async isAuthenticated(): Promise<boolean> {
     const token = localStorage.getItem('token');
-    if(token) {
-
-      if(!this.jwtHelper.isTokenExpired(token)){
-
-        this.httpClient.get<UserModel>(`${this.baseUrl}/admin/user/details`,
-          { observe: 'response', headers: {'Authorization': "Bearer " + token }})
-          .subscribe(response => {
+    if (token && !this.jwtHelper.isTokenExpired(token)) {
+      return lastValueFrom(this.httpClient.get<UserModel>(`${this.baseUrl}/admin/user/details`,
+        {observe: 'response', headers: {'Authorization': "Bearer " + token}})
+        .pipe(map(response => {
           let body = response.body!;
           this.userModel.id = body.id;
           this.userModel.username = body.username;
           this.userModel.fullName = body.fullName;
-          if(response.status == HttpStatusCode.Ok){
-            return true;
-          }
-          return false;
-        });
-      }
-      else {
-        return false;
-      }
-      //TODO am Backend nach validität des Users Fragen
-
+          return true;
+        }), catchError(error => of(false))));
     } else {
       return false;
     }
   }
-
 
 
 }
